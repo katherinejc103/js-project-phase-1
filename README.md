f347aa1a276266d06d472ba6ff2e79ee34180106
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-04-16 16:39:40 -0400

    Done

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Javascript Project Phase One
 
 ### Overview
+This webpage serves as a checkin for users that want to being their yoga journey. The most difficult aspect of flexibility is consitency, so the page serves as a reminder to begin your morning with a few stretches and incorporates mindfullness checkin with a daily meditation/ mantra that is consistent.
 
 
-### How to install and use 
+### How to install and use
+The user can click the links on the nav bar to display the list of beginner friendly poses by clicking on the "yoga poses" or display the daily mantra/meditation by clicking on the "daily meditation" which appears as an alert because its function is to ponder and reflect upon the quote daily so that it becomes a part of the users lifestyle/ mindset.
 
 
 ### How to contribute
+the user can contribute by clicking the submit button to acknowledge their completion of their "morning yoga" and proceed with their day.
--- main.js
+++ main.js
@@ -2,20 +2,13 @@
 const baseUrl = "http://localhost:3000";
 let poses = [];
 
-
 /*****NODE Getters ****/
 const mainDiv = () => document.getElementById("main");
 
-/**Templates**/
-// const homePageTemplate = () => {
-//     return `
-//     <h1 class="center-align"></h1>
-//     `
-// }
 
-/***Renders*****/
-renderHomePage = () => {
-    mainDiv().innerHTML = homePageTemplate();
+const renderTable = () => {
+    mainDiv().innerHTML = createTable();
+    console.log('hello')
 }
 
 poses.forEach(pose => {
@@ -27,6 +20,39 @@ poses.forEach(pose => {
 })
 
 
+
+const createTable = () => { 
+    console.log(renderPoses())
+    return `
+    <table class="container">
+            <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>Position</th>
+                  <th>level</th>
+              </tr>
+            </thead>
+    
+            <tbody>
+              ${ renderPoses() }
+            </tbody>
+          </table> `
+
+} 
+
+const poseTemplete = (poses) => {
+    return `
+        <tr>
+            <td>${ poses.name }</td>
+            <td>${ poses.position }</td>
+            <td>${ poses.level }</td>
+        </tr>
+    `
+}
+
+
+
+
 /******Events********/
 const loadPoses = () => {
     fetch(baseUrl + '/poses')
@@ -34,30 +60,65 @@ const loadPoses = () => {
         .then(data => {
             console.log('data', data) 
             poses = data;
+            
+    renderTable();
         })
 }
 
-// const submitMindfullness = document.getElementBy
-
 const posesLink = () => document.getElementById("poses-link");
+const medLink = () => document.getElementById("med-link");
 
 const renderPosePage = (e) => {
     e.preventDefault();
+    resetMain();
 
-    alert("it works");
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const h5 = document.createElement('h5');
+
+    h3.innerText = "Yoga Pose Positions" 
+    h4.innerText = "You may ask why yoga is essential to a balanced lifeystyle?"
+    h5.innerText = "and it can be as easily defined as: a Hindu spiritual and ascetic discipline, a part of which, including breath control, simple meditation, and the adoption of specific bodily postures, is widely practiced for health and relaxation."
+    mainDiv().appendChild(h3);
+    mainDiv().appendChild(h4);
+    mainDiv().appendChild(h5);
+
+    
+}
+
+const renderMeditation = (e) => {
+    e.preventDefault();
+   
+    alert("When your world moves too fast and you lose yourself in the chaos, introduce yourself to each color of the sunset. Reacquaint yourself with the earth beneath your feet. Thank the air that surrounds you with every breath you take. Find yourself in the appreciation of life.” ~Christy Ann Martine")
+}
+
+const renderPoses = () => {
+    console.log(poses)
+    return poses.map(pose => poseTemplete(pose));
+}
+
+const attachMedLinkEvent = () => {
+    medLink().addEventListener('click', renderMeditation);
 }
 
 const attachPosesLinkEvent = () => {
     posesLink().addEventListener('click', renderPosePage);
 }
 
-
+const resetMain = () => {
+    mainDiv().innerHTML = "";
+}
 
 
  
 /*******WHEN THE DOM LOADS*******/
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('hi')
     attachPosesLinkEvent();
+    attachMedLinkEvent();
     loadPoses();
-    renderPosePage();
+    // renderPosePage();
+    // renderMeditation();
+    console.log('hello')
 })
+
